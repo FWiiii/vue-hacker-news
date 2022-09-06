f7e1e9ae7c984ecfc6e3555a4efe5476062ce31c
FWiiii <843189132@qq.com>
2022-09-06 16:30:53 +0800

    chore:del vercel.json

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import vue from "@vitejs/plugin-vue";
 import eslint from "vite-plugin-eslint";
 import { resolve } from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
@@ -18,14 +17,4 @@ export default defineConfig({
       "@": resolve(__dirname, "src")
     }
   },
-  // server: { //主要是加上这段代码
-  //   proxy: {
-  //     '/api': {
-  //       target: 'https://hn.nuxtjs.org/api',	//实际请求地址
-  //       changeOrigin: true,
-  //       rewrite: (path) => path.replace(/^\/api/, '')
-  //     },
-  //   }
-  // }
-
 });
